5084fa46e2d9f4dfd6fdad1ab7497cf01c762bee
demesameneshoa <demesameneshoa@gmail.com>
2023-07-11 23:33:27 +0300

    add routes and navigation  bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Quote from './components/Quotes';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <div className="Calculator-container">
-        <Calculator />
+const App = () => (
+  <>
+    <nav style={{ display: 'grid', gridTemplateColumns: 'auto auto', width: '90vw' }}>
+      <div>
+        <h1> Math-Magicians</h1>
       </div>
-      <div className="quote-container">
-        <Quote />
+      <div style={{ justifySelf: 'flex-end' }}>
+        <ul>
+          <li>
+            <Link to="/Math-Magicians/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Math-Magicians/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/Math-Magicians/quote">Quote</Link>
+          </li>
+        </ul>
       </div>
-    </>
-  );
-}
+    </nav>
+    <Routes>
+      <Route path="/Math-Magicians/" element={<Home />} />
+      <Route path="/Math-Magicians/calculator" element={<Calculator />} />
+      <Route path="/Math-Magicians/quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;
