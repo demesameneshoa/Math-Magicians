bf9e56cc614bd6c19fff4ca3f025bb1df4d4ac40
demesameneshoa <demesameneshoa@gmail.com>
2023-07-13 00:48:41 +0300

    add button panel test

--- src/tests/ButtonPanel.test.js
+++ src/tests/ButtonPanel.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom/extend-expect';
+import { screen, render } from '@testing-library/react';
 import ButtonPanel from '../components/ButtonPanel';
 
-describe('Calculator component unit test', () => {
-  test('Calculator Component Renders Properly ', () => {
+describe('Buttonpanel component unit test', () => {
+  test('Button Panel Component Renders Properly ', () => {
     const tree = renderer.create(<ButtonPanel />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  test('The Buttons In the Component are 19 ', () => {
+    render(<ButtonPanel />);
+    expect(screen.getAllByRole('button').length).toBe(19);
   });
 });
