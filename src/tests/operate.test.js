e3b85cda68e9cce4a73812499f986600e9c307ee
mdg0410 <levoyersteven@gmail.com>
2023-07-12 16:08:34 -0500

    Add more tests `operate.test.js`

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -17,7 +17,21 @@ describe('testing mathematical operations', () => {
     expect(operate(20, 4, '÷')).toBe('5');
   });
 
+  test('zero division test', () => {
+    expect(operate(18, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+
   test('mod test', () => {
     expect(operate(14, 3, '%')).toBe('2');
+  });
+
+  test('zero mod test', () => {
+    expect(operate(14, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+
+  test('throws error for unknown operation', () => {
+    expect(() => {
+      operate('2', '3', '^');
+    }).toThrow("Unknown operation '^'");
   });
 });
