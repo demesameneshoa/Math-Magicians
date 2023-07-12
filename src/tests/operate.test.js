72917def02b502bcd17a22d5d25608cb91d76284
mdg0410 <levoyersteven@gmail.com>
2023-07-12 15:21:40 -0500

    Add operator tests

---
+++ src/tests/operate.test.js
@@ -0,0 +1,23 @@
+import operate from '../components/logic/operate';
+
+describe('testing mathematical operations', () => {
+  test('addition test', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  test('subtraction test', () => {
+    expect(operate(4, 2, '-')).toBe('2');
+  });
+
+  test('multiplication test', () => {
+    expect(operate(5, 3, '*')).toBe('15');
+  });
+
+  test('division test', () => {
+    expect(operate(20, 4, '/')).toBe('5');
+  });
+
+  test('mod test', () => {
+    expect(operate(14, 3, '%')).toBe('2');
+  });
+});
